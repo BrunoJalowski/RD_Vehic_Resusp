f03b85d6b7e22dcbc79b8562c55a8a58691fdd32
Bruno Jalowski <bruno.jalowski@gmail.com>
2025-03-28 10:16:26 -0300

    atualizacao scripts

--- Scripts/emissions.py
+++ Scripts/emissions.py
@@ -12,33 +12,68 @@ import numpy as np
 from main import gdf_filtered
 import emission_factors as ef
 
-#%%Dados iniciais via não pavimentada
+#%% EMISSÕES VIAS NÃO PAVIMENTADAS ACESSO PÚBLICO
+
+#Dados iniciais via não pavimentada
 silt_fraction = 1
-pm = 2.5    # 
+
+### PM2.5
+#Fator de emissão PM2.5 em lb/VMT
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25'] = ef.emission_unpaved_public(2.5, silt_fraction, (gdf_filtered.loc[:,'traffic_level'] * 0.621371), gdf_filtered.loc[:,'soil_moisture'])
+
+    #0.621371 é o fator de conversão de km/h para mph
+
+#Conversão de lb/VMT para g/VKT
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25'] = gdf_filtered.loc[:,'EF25'].apply(ef.lbvmt_to_gvkt)
+
+#Correção da emissão pela pluviosidade
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25_corrected'] = ef.unpaved_rainfall_correction(gdf_filtered['EF25'], 100, 365)
+
+#Emissão de Pm2.5 por trecho de via
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','25_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='unpaved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25_corrected'] 
 
 
-#%%Fator de emissão PM2.5 por trecho de via não pavimentada de acesso público em lb/VMT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF_unpaved'] = ef.emission_unpaved_public(pm, silt_fraction, (gdf_filtered.loc[:,'traffic_level'] * 0.621371), gdf_filtered.loc[:,'soil_moisture'])
+### PM10
+#Fator de emissão PM10 em lb/VMT
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10'] = ef.emission_unpaved_public(10, silt_fraction, (gdf_filtered.loc[:,'traffic_level'] * 0.621371), gdf_filtered.loc[:,'soil_moisture'])
 
-# 0.621371 é o fator de conversão de km/h para mph
+    #0.621371 é o fator de conversão de km/h para mph
 
-#%%Conversão de lb/VMT para g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF_unpaved'] = gdf_filtered.loc[:,'EF_unpaved'].apply(ef.lbvmt_to_gvkt)
+#Conversão de lb/VMT para g/VKT
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10'] = gdf_filtered.loc[:,'EF10'].apply(ef.lbvmt_to_gvkt)
 
-#%%Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF_unpaved_corrected'] = ef.paved_rainfall_correction(gdf_filtered['EF_unpaved'], 100, 365)
+#Correção da emissão pela pluviosidade
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10_corrected'] = ef.unpaved_rainfall_correction(gdf_filtered['EF10'], 100, 365)
 
-#%%Emissão de Pm2.5 por trecho de via
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','emission'] = gdf_filtered.loc[gdf_filtered['surface']=='unpaved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF_unpaved_corrected'] 
-
-
-#%%Mostrar a distribuição das emissões no espaço
+#Emissão de Pm10 por trecho de via
+gdf_filtered.loc[gdf_filtered['surface']=='unpaved','10_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='unpaved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10_corrected'] 
 
 
+#%% EMISSÕES VIAS PAVIMENTADAS
+
+# Dados iniciais
+weight = 2.4 
+
+### PM2.5
+#Fator de emissão PM2.5 em g/VKT
+gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25'] = ef.emission_paved_roads(2.5, gdf_filtered.loc[:,'silt_loading'], weight)
+
+#Correção da emissão pela pluviosidade
+gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25_corrected'] = ef.paved_rainfall_correction(gdf_filtered['EF25'], 100, 365)
+
+#Emissão de Pm2.5 por trecho de via
+gdf_filtered.loc[gdf_filtered['surface']=='paved','25_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='paved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='paved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25_corrected'] 
 
 
+### PM10
+#Fator de emissão PM10 em g/VKT
+gdf_filtered.loc[gdf_filtered['surface']=='paved','EF10'] = ef.emission_paved_roads(10, gdf_filtered.loc[:,'silt_loading'], weight)
 
+#Correção da emissão pela pluviosidade
+gdf_filtered.loc[gdf_filtered['surface']=='paved','EF10_corrected'] = ef.paved_rainfall_correction(gdf_filtered['EF25'], 100, 365)
 
+#Emissão de Pm2.5 por trecho de via
+gdf_filtered.loc[gdf_filtered['surface']=='paved','10_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='paved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='paved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25_corrected'] 
 
 
 
--- Scripts/soil_moisture.py
+++ Scripts/soil_moisture.py
@@ -8,9 +8,6 @@ Created on Fri Mar 21 11:17:19 2025
 Dados de umidade do solo do CMIP da WRF. 
 A variável de interesse é SOIM1 = volumetric soil moisture in near-surface soil (m3.m-3).
 
-
-
-
 """
 #%% Importando pacotes
 import xarray as xr
@@ -42,20 +39,22 @@ plt.show()
 #%%Designando valores de umidade do solo para cada trecho de via
 values = []
 for _, row in gdf_filtered.iterrows():
-    line = row['geometry']  # Access the LineString geometry
+    line = row['geometry']  
 
-    if line.geom_type == 'LineString':  # Check if the geometry is a LineString
-        line_values = []  # List to store pixel values for this LineString
-        for point in line.coords:  # Loop through each point in the LineString
+    if line.geom_type == 'LineString':
+        line_values = []  
+        """ Para cada ponto na LineString, pega os índices mais próximos e com 
+         eles o valor de umidade"""
+        for point in line.coords:
             lon, lat = point
-            lat_idx = np.abs(soil_moisture['lat'] - lat).argmin()  # Nearest latitude index
-            lon_idx = np.abs(soil_moisture['lon'] - lon).argmin()  # Nearest longitude index
-            value = soil_moisture[0,0, lat_idx, lon_idx].values  # Get pixel value (for all times)
+            lat_idx = np.abs(soil_moisture['lat'] - lat).argmin()  
+            lon_idx = np.abs(soil_moisture['lon'] - lon).argmin()  
+            value = soil_moisture[0,0, lat_idx, lon_idx].values  
             line_values.append(value)
-        values.append(line_values)  # Add the pixel values of the current line to the list
+        values.append(line_values)  
 
     else:
-        values.append(None)  # If it's not a LineString, append None
+        values.append(None)  
 
 
 gdf_filtered['soil_moisture'] = values
@@ -63,14 +62,63 @@ gdf_filtered.loc[:,'soil_moisture'] = gdf_filtered.loc[:,'soil_moisture'].str[0]
 
 del lat, lat_idx, line, line_values,lon,lon_idx,point,row
 
+
+
+
+
 #%%Plotando para ver localização das vias nao pavimentadas
-fig, ax = plt.subplots()
+"""fig, ax = plt.subplots()
 xr.plot.pcolormesh(darray=soil_moisture[0,0,:,:],ax=ax)
 gdf_filtered.loc[gdf_filtered['surface']=='unpaved',:].plot(ax=ax, color="r")
 
-plt.show()
+plt.show()"""
 
 """Ao menos na amostra, as vias pavimentadas se apresentam como maioria no litoral.
 Desse modo, talvez essa resolução espacial utilizada não afete muito a estimativa das
 emissoes em vias nao pavimentadas, já que só elas que usam umidade do solo"""
 
+
+
+#%%
+
+def soil_moisture(gdf,xds):
+    #Abrindo o dataset do CMIP
+    xds = xr.open_mfdataset('/home/brunojalowski/Documentos/RD_Vehic_Resusp/dados_entrada/Soil Moisture/METCRO2D_BR_20km_2023-02-01.nc')
+        
+    #Definição das coordenadas LAT e LON do dataset 
+    xds = conv.brain_to_latlng(xds) 
+    
+    #Localizar a variável de soil moisture
+    soil_moisture = xds['SOIM1'] #SOIM1 = volumetric soil moisture in near-surface soil (m3.m-3)
+    
+    #Passando o geodataframe para o mesmo CRS de soil_moisture
+    gdf = gdf.to_crs(soil_moisture.rio.crs)
+    
+    
+    #Designando valores de umidade do solo para cada trecho de via
+    values = []
+    for _, row in gdf.iterrows():
+        line = row['geometry']  
+    
+        if line.geom_type == 'LineString':
+            line_values = []  
+            """ Para cada ponto na LineString, pega os índices mais próximos e com 
+             eles o valor de umidade"""
+            for point in line.coords:
+                lon, lat = point
+                lat_idx = np.abs(soil_moisture['lat'] - lat).argmin()  
+                lon_idx = np.abs(soil_moisture['lon'] - lon).argmin()  
+                value = soil_moisture[0,0, lat_idx, lon_idx].values  
+                line_values.append(value)
+            values.append(line_values)  
+    
+        else:
+            values.append(None)  
+    
+    
+    gdf['soil_moisture'] = values
+    gdf.loc[:,'soil_moisture'] = gdf.loc[:,'soil_moisture'].str[0]
+    
+    del lat, lat_idx, line, line_values,lon,lon_idx,point,row
+    
+    return gdf 
