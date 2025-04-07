7a2a407bf8b57778fd6dbf7792121111599d6e98
Bruno Jalowski <bruno.jalowski@gmail.com>
2025-04-07 13:33:30 -0300

    emissoes com silt fraction

--- Scripts/silt_fraction.py
+++ Scripts/silt_fraction.py
@@ -35,7 +35,7 @@ silt_fraction.rio.write_crs("epsg:4326", inplace=True)
 
 #%% Downscaling
 
-# Reduzindo a dimensão do raster 1/5
+# Reduzindo a dimensão do raster 1/10
 downscale_factor = 1/10
     
 # nova largura e altura
@@ -87,7 +87,7 @@ for _, row in gdf_filtered.iterrows():
 gdf_filtered['silt_fraction'] = values
 gdf_filtered.loc[:,'silt_fraction'] = gdf_filtered.loc[:,'silt_fraction'].str[0]
 
-#del lat, lat_idx, line, line_values,lon,lon_idx,point,row
+del lat, lat_idx, line, line_values,lon,lon_idx,point,row
 
 #%% Assignment results
 
@@ -102,3 +102,51 @@ ax.set_ylim(miny-0.01, maxy+0.01)
 ax.set_title('Silt Fraction by road')
 ax.set_xlabel('Longitude')
 ax.set_ylabel('Latitude')
+
+
+#%%
+def silt_fraction(gdf, raster):
+    raster = rxr.open_rasterio(silt_fraction_path / 'mapbiomas-brazil-collection2-beta-000_010cm-granulometry_silt_percent-0000095232-0000063488.tif', band_as_variable=True)
+    
+    # Definindo CRS
+    raster.rio.write_crs("epsg:4326", inplace=True)
+    
+    # Reduzindo a dimensão do raster 1/10
+    downscale_factor = 1/10
+        
+    # nova largura e altura
+    new_width = raster.rio.width * downscale_factor
+    new_height = raster.rio.height * downscale_factor
+        
+    # fazendo o downscaling
+    raster = raster.rio.reproject(raster.rio.crs, shape=(int(new_height),
+                                                         int(new_width)),
+                                  resampling=Resampling.bilinear)
+    
+    #Designando valores de teor de silte para cada trecho de via
+    values = []
+    for _, row in gdf.iterrows():
+        line = row['geometry']  
+    
+        if line.geom_type == 'LineString':
+            line_values = []  
+            """ Para cada ponto na LineString, pega os índices mais próximos e com 
+             eles o teor de silte"""
+            for point in line.coords:
+                lon, lat = point
+                lat_idx = np.abs(raster['y'] - lat).argmin()  
+                lon_idx = np.abs(raster['x'] - lon).argmin()  
+                value = raster['band_1'][lon_idx, lat_idx].values  
+                line_values.append(value)
+            values.append(line_values)  
+    
+        else:
+            values.append(None)  
+    
+    
+    gdf['silt_fraction'] = values
+    gdf.loc[:,'silt_fraction'] = gdf.loc[:,'silt_fraction'].str[0]
+    
+    del lat, lat_idx, line, line_values,lon,lon_idx,point,row
+    
+    return gdf
