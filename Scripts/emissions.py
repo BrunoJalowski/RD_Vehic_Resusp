93712d16a730d84febd64e073bd77c2bcf0e189f
Bruno Jalowski <bruno.jalowski@gmail.com>
2025-06-30 15:53:00 -0300

    versao do main lendo varios arquivos

--- Scripts/emissions.py
+++ Scripts/emissions.py
@@ -5,94 +5,178 @@ Created on Fri Mar 21 11:46:24 2025
 
 @author: brunojalowski
 """
-from main import gdf_filtered
+from main import gdf_filtered, vehicular_weight
 import emission_factors as ef
 
-#%% EMISSÕES VIAS NÃO PAVIMENTADAS ACESSO PÚBLICO
+# %% EMISSÕES VIAS NÃO PAVIMENTADAS ACESSO PÚBLICO
 
-#Dados iniciais via não pavimentada
+# Dados iniciais via não pavimentada
 
-### PM2.5
-#Fator de emissão PM2.5 em lb/VMT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25'] = ef.emission_unpaved_public(2.5, gdf_filtered.loc[:,'silt_fraction'], (gdf_filtered.loc[:,'traffic_level'] * 0.621371), gdf_filtered.loc[:,'soil_moisture'])
+# ## PM2.5
+# Fator de emissão PM2.5 em lb/VMT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF25']
+) = ef.emission_unpaved_public(2.5, gdf_filtered.loc[:, 'silt_fraction'],
+                               (gdf_filtered.loc[:, 'traffic_level'] *
+                                0.621371),  # conversao km/h para mph
+                               gdf_filtered.loc[:, 'soil_moisture'])
 
-    #0.621371 é o fator de conversão de km/h para mph
+# Conversão de lb/VMT para g/VKT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF25']
+) = gdf_filtered.loc[:, 'EF25'].apply(ef.lbvmt_to_gvkt)
 
-#Conversão de lb/VMT para g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25'] = gdf_filtered.loc[:,'EF25'].apply(ef.lbvmt_to_gvkt)
+# Correção da emissão pela pluviosidade
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF25_corrected']
+) = ef.unpaved_rainfall_correction(gdf_filtered['EF25'], 0, 365)
 
-#Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25_corrected'] = ef.unpaved_rainfall_correction(gdf_filtered['EF25'], 0, 365)
-
-#Taxa de emissão de Pm2.5 por trecho de via (g/s)
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','25_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='unpaved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF25_corrected'] / 3600
-
-
-### PM10
-#Fator de emissão PM10 em lb/VMT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10'] = ef.emission_unpaved_public(10, gdf_filtered.loc[:,'silt_fraction'], (gdf_filtered.loc[:,'traffic_level'] * 0.621371), gdf_filtered.loc[:,'soil_moisture'])
-
-    #0.621371 é o fator de conversão de km/h para mph
-
-#Conversão de lb/VMT para g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10'] = gdf_filtered.loc[:,'EF10'].apply(ef.lbvmt_to_gvkt)
-
-#Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10_corrected'] = ef.unpaved_rainfall_correction(gdf_filtered['EF10'], 0, 365)
-
-#Taxa de emissão de Pm10 por trecho de via (g/s)
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','10_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='unpaved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF10_corrected'] / 3600
+# Taxa de emissão de Pm2.5 por trecho de via (g/s)
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', '25_emission']
+) = (gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'flow'] *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'length'] /
+     1000 *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF25_corrected'] /
+     3600)
 
 
-### PM30
-#Fator de emissão PM30 em lb/VMT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF30'] = ef.emission_unpaved_public(30, gdf_filtered.loc[:,'silt_fraction'], (gdf_filtered.loc[:,'traffic_level'] * 0.621371), gdf_filtered.loc[:,'soil_moisture'])
+# ## PM10
+# Fator de emissão PM10 em lb/VMT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF10']
+) = ef.emission_unpaved_public(10,
+                               gdf_filtered.loc[:, 'silt_fraction'],
+                               (gdf_filtered.loc[:, 'traffic_level'] *
+                                0.621371),  # conversao km/h para mph
+                               gdf_filtered.loc[:, 'soil_moisture'])
 
-    #0.621371 é o fator de conversão de km/h para mph
+# Conversão de lb/VMT para g/VKT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF10']
+) = gdf_filtered.loc[:, 'EF10'].apply(ef.lbvmt_to_gvkt)
 
-#Conversão de lb/VMT para g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF30'] = gdf_filtered.loc[:,'EF30'].apply(ef.lbvmt_to_gvkt)
+# Correção da emissão pela pluviosidade
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF10_corrected']
+) = ef.unpaved_rainfall_correction(gdf_filtered['EF10'], 0, 365)
 
-#Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF30_corrected'] = ef.unpaved_rainfall_correction(gdf_filtered['EF30'], 0, 365)
+# Taxa de emissão de Pm10 por trecho de via (g/s)
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', '10_emission']
+) = (gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'flow'] *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'length'] /
+     1000 *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF10_corrected'] /
+     3600)
 
-#Taxa de Emissão de Pm30 por trecho de via (g/s)
-gdf_filtered.loc[gdf_filtered['surface']=='unpaved','30_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='unpaved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='unpaved','EF30_corrected'] / 3600 
 
-#%% EMISSÕES VIAS PAVIMENTADAS
+# ## PM30
+# Fator de emissão PM30 em lb/VMT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF30']
+) = ef.emission_unpaved_public(30,
+                               gdf_filtered.loc[:, 'silt_fraction'],
+                               (gdf_filtered.loc[:, 'traffic_level'] *
+                                0.621371),  # conversao km/h para mph
+                               gdf_filtered.loc[:, 'soil_moisture'])
+
+# Conversão de lb/VMT para g/VKT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF30']
+) = gdf_filtered.loc[:, 'EF30'].apply(ef.lbvmt_to_gvkt)
+
+
+# Correção da emissão pela pluviosidade
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF30_corrected']
+) = ef.unpaved_rainfall_correction(gdf_filtered['EF30'], 0, 365)
+
+# Taxa de Emissão de Pm30 por trecho de via (g/s)
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', '30_emission']
+) = (gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'flow'] *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'length'] /
+     1000 *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'unpaved', 'EF30_corrected'] /
+     3600)
+
+# %% EMISSÕES VIAS PAVIMENTADAS
 
 # Dados iniciais
-weight = 2.4 
+weight = vehicular_weight.loc[vehicular_weight['MUNICIPIO'] == 'FLORIANOPOLIS',
+                              "average_weight"]
 
-### PM2.5
-#Fator de emissão PM2.5 em g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25'] = ef.emission_paved_roads(2.5, gdf_filtered.loc[:,'silt_loading'], weight)
+# ## PM2.5
+# Fator de emissão PM2.5 em g/VKT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF25']
+) = ef.emission_paved_roads(2.5,
+                            gdf_filtered.loc[:, 'silt_loading'],
+                            weight)
 
-#Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25_corrected'] = ef.paved_rainfall_correction(gdf_filtered['EF25'], 0, 365)
+# Correção da emissão pela pluviosidade
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF25_corrected']
+) = ef.paved_rainfall_correction(gdf_filtered['EF25'],
+                                 0,
+                                 365)
 
-#Taxa de emissão de Pm2.5 por trecho de via (g/s)
-gdf_filtered.loc[gdf_filtered['surface']=='paved','25_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='paved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='paved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='paved','EF25_corrected'] / 3600
+# Taxa de emissão de Pm2.5 por trecho de via (g/s)
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', '25_emission']
+) = (gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'flow'] *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'length'] /
+     1000 *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF25_corrected'] /
+     3600)
 
 
-### PM10
-#Fator de emissão PM10 em g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='paved','EF10'] = ef.emission_paved_roads(10, gdf_filtered.loc[:,'silt_loading'], weight)
+# ## PM10
+# Fator de emissão PM10 em g/VKT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF10']
+) = ef.emission_paved_roads(10,
+                            gdf_filtered.loc[:, 'silt_loading'],
+                            weight)
 
-#Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='paved','EF10_corrected'] = ef.paved_rainfall_correction(gdf_filtered['EF10'], 0, 365)
+# Correção da emissão pela pluviosidade
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF10_corrected']
+) = ef.paved_rainfall_correction(gdf_filtered['EF10'],
+                                 0,
+                                 365)
 
-#Taxa de emissão de Pm10 por trecho de via (g/s)
-gdf_filtered.loc[gdf_filtered['surface']=='paved','10_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='paved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='paved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='paved','EF10_corrected'] / 3600
+# Taxa de emissão de Pm10 por trecho de via (g/s)
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', '10_emission']
+) = (gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'flow'] *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'length'] /
+     1000 *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF10_corrected'] /
+     3600)
 
 
-### PM30
-#Fator de emissão PM30 em g/VKT
-gdf_filtered.loc[gdf_filtered['surface']=='paved','EF30'] = ef.emission_paved_roads(30, gdf_filtered.loc[:,'silt_loading'], weight)
+# ## PM30
+# Fator de emissão PM30 em g/VKT
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF30']
+) = ef.emission_paved_roads(30,
+                            gdf_filtered.loc[:, 'silt_loading'],
+                            weight)
 
-#Correção da emissão pela pluviosidade
-gdf_filtered.loc[gdf_filtered['surface']=='paved','EF30_corrected'] = ef.paved_rainfall_correction(gdf_filtered['EF30'], 0, 365)
+# Correção da emissão pela pluviosidade
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF30_corrected']
+) = ef.paved_rainfall_correction(gdf_filtered['EF30'],
+                                 0,
+                                 365)
 
-#Taxa de Emissão de Pm30 por trecho de via (g/s)
-gdf_filtered.loc[gdf_filtered['surface']=='paved','30_emission'] = gdf_filtered.loc[gdf_filtered['surface']=='paved','flow'] * gdf_filtered.loc[gdf_filtered['surface']=='paved','length']/1000 * gdf_filtered.loc[gdf_filtered['surface']=='paved','EF30_corrected'] / 3600
-
+# Taxa de Emissão de Pm30 por trecho de via (g/s)
+(
+    gdf_filtered.loc[gdf_filtered['surface'] == 'paved', '30_emission']
+) = (gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'flow'] *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'length'] /
+     1000 *
+     gdf_filtered.loc[gdf_filtered['surface'] == 'paved', 'EF30_corrected'] /
+     3600)
--- Scripts/vehicular_weight.py
+++ Scripts/vehicular_weight.py
@@ -11,12 +11,12 @@ import geopandas as gpd
 import numpy as np
 import unidecode
 
-path = '/home/brunojalowski/Downloads/emissoes_evaporativas_diurnal_hot_runningV2/2.FrotaPorMunicipio'
-filename = f'{path}/frota_munic_modelo_janeiro_2019.xls'
+path = '/home/brunojalowski/Documentos/RD_Vehic_Resusp/dados_entrada'
+filename = f'{path}/FrotapormunicipioetipoDezembro2024.xlsx'
 
 df = pd.read_excel(filename, skiprows=3)
 
-#%% Reclassificação categorias de veículos
+# %% Reclassificação categorias de veículos
 """
 Classificação atual:
     Index(['UF', 'MUNICIPIO', 'TOTAL', 'AUTOMOVEL', 'BONDE', 'CAMINHAO',
@@ -28,18 +28,19 @@ Classificação atual:
 
 Reclassificação:
     leves = AUTOMOVEL, BONDE, CAMINHONETE, CAMIONETA, UTILITARIO, OUTROS
-    motocicletas = CICLOMOTOR, MOTOCICLETA, MOTONETA, QUADRICICLO, SIDE-CAR, TRICICLO
-    pesados = CAMINHAO, CAMINHAO TRATOR, CHASSI PLATAF, MICRO-ONIBUS, ONIBUS, REBOQUE,
-            SEMI-REBOQUE, TRATOR ESTEI, TRATOR RODAS
+    motocicletas = CICLOMOTOR, MOTOCICLETA, MOTONETA, QUADRICICLO, SIDE-CAR,
+                   TRICICLO
+    pesados = CAMINHAO, CAMINHAO TRATOR, CHASSI PLATAF, MICRO-ONIBUS, ONIBUS,
+              REBOQUE, SEMI-REBOQUE, TRATOR ESTEI, TRATOR RODAS
 
 """
 
 df['leves'] = df.loc[:, ['AUTOMOVEL',
-                          'BONDE',
-                          'CAMINHONETE',
-                          'CAMIONETA',
-                          'UTILITARIO',
-                          'OUTROS']].sum(1)
+                         'BONDE',
+                         'CAMINHONETE',
+                         'CAMIONETA',
+                         'UTILITARIO',
+                         'OUTROS']].sum(1)
 
 df['motocicletas'] = df.loc[:, ['CICLOMOTOR',
                                 'MOTOCICLETA',
@@ -60,124 +61,75 @@ df['pesados'] = df.loc[:, ['CAMINHAO',
 
 #%% Pesos das categorias de veículo (toneladas)
 
-peso_leves =  1.1485  #temporário, ainda falta somar peso do motorista
-peso_motos =  0.128
-peso_pesados = 16.0 
+peso_leves = 1.1485  # temporário, ainda falta somar peso do motorista
+peso_motos = 0.128
+peso_pesados = 16.0
 
-df['peso_medio'] = (df.loc[:,'leves'] * peso_leves +
-                    df.loc[:,'motocicletas'] * peso_motos +
-                    df.loc[:,'pesados'] * peso_pesados) / df.loc[:,'TOTAL']
+df['peso_medio'] = (df.loc[:, 'leves'] * peso_leves +
+                    df.loc[:, 'motocicletas'] * peso_motos +
+                    df.loc[:, 'pesados'] * peso_pesados) / df.loc[:, 'TOTAL']
 
-#%%
+del peso_leves, peso_motos, peso_pesados
 
-caminho_diretorio = r"/home/brunojalowski/Downloads/emissoes_evaporativas_diurnal_hot_runningV2"
+# %% FUNÇÃO PESO MÉDIO VEICULAR
 
-def identificando_cod_ibge(caminho_diretorio):
+def vehicular_weight(fleet_path: str,
+                     light_duty_weight = 1.1485,
+                     heavy_duty_weight = 16.0,
+                     motorcycle_weight = 0.128):
     """
-    Processa e extrai informações dos códigos do IBGE para estados e municípios 
-    do Brasil
+    This function calculates de average vehicular weight for each city based 
+    on fleet composition and median weight for each vehicle category
 
-    Parâmetros:
-    caminho_diretorio (str): Caminho do diretório onde estão localizados os arquivos
+    Parameters
+    ----------
+    fleet_path : str
+        DESCRIPTION.
+    lightduty_weight : int or float, optional
+        DESCRIPTION. The default is 1.1485.
+    heavyduty_weight : int or float, optional
+        DESCRIPTION. The default is 16.0.
+    motorcycle_weight : int or float, optional
+        DESCRIPTION. The default is 0.128.
 
-    Retorna:
-        list[tuple]: Lista de tuplas contendo (nome do município, código do município IBGE).
-        list[tuple]: Lista de tuplas contendo (nome do estado, código do estado IBGE).
-        list[str]: Lista de siglas das unidades federativas (UF).
-        numpy.ndarray: Array com os códigos numéricos das unidades federativas.
+    Returns
+    -------
+    df : TYPE
+        DESCRIPTION.
+
     """
-    
-    # Leitura do arquivo Excel
-    filename = f'{caminho_diretorio}/RELATORIO_DTB_BRASIL_MUNICIPIO.xls'
-    try:
-        df = pd.read_excel(filename, skiprows=6)  # Pula as primeiras 6 linhas
-    except Exception as e:
-        print(f"Erro ao ler o arquivo Excel: {e}")
-        return None, None, None, None
-    
-    # Renomeação das colunas
-    df = df.rename(columns={
-        'UF': 'cod_uf',
-        'Nome_UF': 'nome_uf',
-        'Código Município Completo': 'cod_municipio',
-        'Nome_Município': 'nome_municipio'})
-    
-    # Criação da lista de municípios
-    IBGE_CITIES = df[['cod_uf', 'nome_uf', 'cod_municipio', 'nome_municipio']].values.tolist()
-    IBGE_CITIES_CODES = df['cod_municipio'].astype(str).values
-    
-    # Leitura do shapefile dos estados
-    try:
-        UFshp = gpd.read_file(f'{caminho_diretorio}/BR_UF_2023/BR_UF_2023.shp')
-    except Exception as e:
-        print(f"Erro ao ler o shapefile dos estados: {e}")
-        return None, None, None, None
-    
-    # Extração de códigos e nomes dos estados
-    codigos_uf = UFshp['CD_UF'].astype(int).values
-    UFnames = UFshp['NM_UF'].values
-    
-    # Dicionário de mapeamento de códigos para siglas
-    codigo_para_sigla = {
-        11: 'RO', 12: 'AC', 13: 'AM', 14: 'RR', 15: 'PA', 16: 'AP', 17: 'TO',
-        21: 'MA', 22: 'PI', 23: 'CE', 24: 'RN', 25: 'PB', 26: 'PE', 27: 'AL',
-        28: 'SE', 29: 'BA', 31: 'MG', 32: 'ES', 33: 'RJ', 35: 'SP', 41: 'PR',
-        42: 'SC', 43: 'RS', 50: 'MS', 51: 'MT', 52: 'GO', 53: 'DF'}
-    
-    # Gerar ibge_uf com base na ordem de codigos_uf
-    ibge_uf = [codigo_para_sigla[codigo] for codigo in codigos_uf]
-    
-    # Leitura dos shapefiles dos municípios
-    try:
-        CITYshpIBGE = gpd.read_file(f'{caminho_diretorio}/BR_Municipios_2023/BR_Municipios_2023.shp')
-    except Exception as e:
-        print(f"Erro ao ler o shapefile dos municípios do IBGE: {e}")
-        return None, None, None, None
-    
-    CITYcodesIBGE = CITYshpIBGE['CD_MUN'].astype(int).values
-    CITYnamesIBGE = CITYshpIBGE['NM_MUN'].values
-    
-    try:
-        CITYshpFOREST = gpd.read_file(f'{caminho_diretorio}/ForestGIS_Cidades_Brasil_pop2021_ibge/ForestGIS_Cidades_Brasil_pop2021_ibge.shp')
-    except Exception as e:
-        print(f"Erro ao ler o shapefile dos municípios do ForestGIS: {e}")
-        return None, None, None, None
-    
-    CITYcodesFOREST = CITYshpFOREST['IBGECd'].astype(int).values
-    CITYnamesFOREST = CITYshpFOREST['Nom_Mun'].values
-    
-    # Comparação de códigos entre diferentes fontes
-    lia1 = np.isin(CITYcodesIBGE, IBGE_CITIES_CODES.astype(int))
-    lia2 = np.isin(CITYcodesIBGE, CITYcodesFOREST)
-    
-    # Identificação de municípios faltantes
-    Falta_Municipios = CITYnamesIBGE[~lia1]
-    Falta_Codigo = CITYcodesIBGE[~lia1]
-    
-    # Correção de nomes de municípios faltantes
-    Falta_Municipios = [unidecode(str(name)) for name in Falta_Municipios]
-    
-    # Adição de municípios faltantes na lista completa
-    codiState = (Falta_Codigo // 10**5).astype(int)
-    loccodes2 = np.searchsorted(codigos_uf, codiState)
-    falta_codiState = UFnames[loccodes2]
-    
-    Municipios_faltantes = list(zip(falta_codiState, codiState, Falta_Codigo, Falta_Municipios))
-    IBGE_CITIES.extend(Municipios_faltantes)
-    
-    # Ordenação da lista completa de municípios
-    IBGE_CITIES.sort(key=lambda x: x[2])
-    
-    # Remoção de caracteres especiais e espaços
-    CITYnames = [unidecode(str(city[3])).upper().replace(' ', '') for city in IBGE_CITIES]
-    
-    # Saída final
-    ibge_dados_cidades = [(city[3], city[2], city[4]) for city in IBGE_CITIES]
-    ibge_estados = list(zip(UFnames, codigos_uf))
-    
-    return ibge_dados_cidades, ibge_estados, ibge_uf, codigos_uf
+    # Reading file containing vehicle fleet
+    df = pd.read_excel(fleet_path, skiprows=3)
 
+    # Reclassifying vehicles
+    df['light_duty'] = df.loc[:, ['AUTOMOVEL',
+                             'BONDE',
+                             'CAMINHONETE',
+                             'CAMIONETA',
+                             'UTILITARIO',
+                             'OUTROS']].sum(1)
 
-ibge_dados_cidades, ibge_estados, ibge_uf, codigos_uf = identificando_cod_ibge(caminho_diretorio)
+    df['motorcycles'] = df.loc[:, ['CICLOMOTOR',
+                                    'MOTOCICLETA',
+                                    'MOTONETA',
+                                    'QUADRICICLO',
+                                    'SIDE-CAR',
+                                    'TRICICLO']].sum(1)
 
-##FALTA ASSOCIAR GEOGRAFICAMENTE,POIS CADA LINHA É UMA CIDADE
+    df['heavy_duty'] = df.loc[:, ['CAMINHAO',
+                               'CAMINHAO TRATOR',
+                               'CHASSI PLATAF',
+                               'MICRO-ONIBUS',
+                               'ONIBUS',
+                               'REBOQUE',
+                               'SEMI-REBOQUE',
+                               'TRATOR ESTEI',
+                               'TRATOR RODAS']].sum(1)
+
+        # Calculating mean_weight for each city
+    df['average_weight'] = ((df.loc[:, 'light_duty'] * light_duty_weight +
+                             df.loc[:, 'motorcycles'] * motorcycle_weight +
+                             df.loc[:, 'heavy_duty'] * heavy_duty_weight) /
+                            df.loc[:, 'TOTAL'])
+
+    return df
